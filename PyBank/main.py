578348758193cfd91ebc545e07b203ada7422820
Josefina Aurea Amaro <josie.a.amaro@gmail.com>
2019-05-10 01:05:25 -0400

    calculated total months - will need adjustment

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,15 +11,22 @@ pybank_data= os.path.join('.','homework_03-Python_Instructions_PyBank_Resources_
 
 # to open csv file
 with open(pybank_data,'r', newline='') as pybankfile:
-    pyBdata_reader= csv.DictReader(pybankfile)
-
+    pyBdata_reader= csv.DictReader(pybankfile, delimiter=',')
     
-    for line in pyBdata_reader:
-       num_months= line['Date'] 
-        #print(num_months)
-
+    #to print the number of months :3
+    list_sum=0
+    for lines in pyBdata_reader:
+        list_sum = list_sum + (len(lines))/2 
+    print("Financial Analysis")
+    print("--------------------------------")
+    print(f'Total Months: {list_sum}')  
+                
+          
+          
+           
+        
+    #source[mergedict]:https://stackoverflow.com/questions/5946236/how-to-merge-multiple-dicts-with-same-key
     #source[csvread&write]: https://www.youtube.com/watch?v=q5uM4VKywbA&t=266s ** helped the most
     #source[csv&python]:https://www.programiz.com/python-programming/working-csv-files 
     #source[readlines]:https://docs.python.org/2/tutorial/inputoutput.html
     #source[to identify columns]:https://stackoverflow.com/questions/16503560/read-specific-columns-from-a-csv-file-with-csv-module 
-  
